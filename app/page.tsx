1f894dd809dc5bb08d94c99ea014a5e4d9311c2a
Ankur Ahir <ahir.ankur0429@gmail.com>
2024-07-20 14:14:18 -0700

    better change

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 import About from "@/components/about";
+import Experience from "@/components/experience";
 import Intro from "@/components/intro";
+import Projects from "@/components/projects";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center px-4">
+    <main className="flex flex-col items-center px-4"> 
       <div className="lg:flex lg:flex-row lg:justify-between lg:gap-10">
         <Intro />
         <About />
       </div>
+      {/* <Experience /> */}
+      <Projects />
     </main>
   );
 }
