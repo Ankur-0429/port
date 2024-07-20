12da55e528253259009956a07fc85e6940234ba7
Ankur Ahir <ahir.ankur0429@gmail.com>
2024-07-20 00:05:57 -0700

    better change

--- lib/data.ts
+++ lib/data.ts
@@ -7,24 +7,16 @@ import WaypointImg from "@/public/WayPointIOS.png";
 
 export const links = [
   {
-    name: "Home",
-    hash: "#home",
-  },
-  {
     name: "About",
     hash: "#about",
   },
   {
-    name: "Projects",
-    hash: "#projects",
-  },
-  {
-    name: "Skills",
-    hash: "#skills",
-  },
-  {
     name: "Experience",
     hash: "#experience",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
   },
   {
     name: "Contact",
